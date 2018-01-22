60d6d37d6327a0728cb91c28646da253d7ee9fae
Fernando Zhu <fernandoisadev@gmail.com>
2018-01-23 00:04:57 +1300

    Added basic GUI to help debugging; setted up basic IRC API endpoints

--- jabber-irc/IRC/IRC.cs
+++ jabber-irc/IRC/IRC.cs
@@ -8,5 +8,115 @@ namespace Jabber.IRC
 {
     class IRC
     {
+        /// <summary>
+        /// Join a channel with its corresponding channel name
+        /// </summary>
+        public void Join()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Me()
+        {
+
+        }
+
+        /// <summary>
+        /// Start a private chat
+        /// </summary>
+        public void Msg()
+        {
+
+        }
+        
+        
+        public void Nick()
+        {
+
+        }
+
+        
+        /// <summary>
+        /// Send a short message to another person without opening up a private window
+        /// </summary>
+        public void Notice()
+        {
+
+        }
+
+        /// <summary>
+        /// To leave a channel
+        /// </summary>
+        public void Part()
+        {
+
+        }
+
+        /// <summary>
+        /// To leave all channels
+        /// </summary>
+        public void PartAll()
+        {
+
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Ping()
+        {
+
+        }
+
+        /// <summary>
+        /// Similar to Msg, except it foreces a window to pop open
+        /// </summary>
+        public void Query()
+        {
+
+        }
+
+        /// <summary>
+        /// Leave IRC altogether
+        /// </summary>
+        public void Quit()
+        {
+
+        }
+
+        /// <summary>
+        /// Ingnore messages sending from a user
+        /// </summary>
+        public void Ignore()
+        {
+
+        }
+
+        /// <summary>
+        /// Check information about one user
+        /// </summary>
+        public void WhoIs()
+        {
+
+        }
+        
+        /// <summary>
+        /// Opens up a DCC/CHAT window to another user
+        /// </summary>
+        public void Chat()
+        {
+
+        }
+
+        /// <summary>
+        /// Check help info
+        /// </summary>
+        public void Help()
+        {
+
+        }
     }
 }
