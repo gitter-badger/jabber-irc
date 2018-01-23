385716e0d10490c3fd94b742e8add5855bf4faeb
Fernando Zhu <fernandoisadev@gmail.com>
2018-01-23 16:34:25 +1300

    Refactored the server connecting request into a separated thread

--- jabber-irc/PreAlpha.xaml.cs
+++ jabber-irc/PreAlpha.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,7 +23,7 @@ namespace jabber_irc
     /// </summary>
     public partial class PreAlpha : Window
     {
-        private string server = "irc.freenode.net";
+        private string server = "chat.freenode.net";
         private int port = 6667;
         private Socket senderSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         private Socket receiverSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -43,13 +45,67 @@ namespace jabber_irc
 
         private void JoinChannel(object sender, RoutedEventArgs e)
         {
-            // Connect to server
-            IPAddress[] remoteIPs = Dns.GetHostAddresses(server);
-            senderSock.Connect(remoteIPs, port);
-            StringBuilder output = new StringBuilder();
-            output.AppendLine(string.Format("Socket connected to {0}", senderSock.RemoteEndPoint.ToString()));
-            debugText.Text = output.ToString();
-            // Join to Channel
+
+            var thread = new Thread(() =>
+            {
+                var client = new TcpClient(server, port);
+                var output = new StringBuilder();
+
+                using (NetworkStream stream = client.GetStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        using (StreamWriter writer = new StreamWriter(stream))
+                        {
+                            this.Dispatcher.Invoke(() =>
+                            {
+                                writer.WriteLine(string.Format("NICK {0}", nicknameText.Text));
+                                writer.Flush();
+                                writer.WriteLine(nicknameText.Text);
+                                writer.Flush();
+                            });
+
+
+                            string inputLine;
+                            while ((inputLine = reader.ReadLine()) != null)
+                            {
+                                this.Dispatcher.Invoke(() =>
+                                {
+                                    output.AppendLine(inputLine);
+                                    debugText.Text = output.ToString();
+                                    Console.WriteLine(inputLine);
+                                });
+
+                                string[] inputWords = inputLine.Split(new Char[] { ' ' });
+                                if (inputWords[0] == "PING")
+                                {
+                                    string pongResponse = inputWords[1];
+                                    writer.WriteLine("PONG " + pongResponse);
+                                    writer.Flush();
+                                }
+
+                                switch (inputWords[1])
+                                {
+                                    case "001":
+                                        writer.WriteLine(string.Format("JOIN {0}", channelText.Text));
+                                        writer.Flush();
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    debugText.Text = output.ToString();
+                });
+
+            });
+
+            thread.Start();
         }
     }
 }
