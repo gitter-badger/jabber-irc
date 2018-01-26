e64205a88b1b141b1276b8bd869c37e8dd74754a
Fernando Zhu <fernandoisadev@gmail.com>
2018-01-26 21:26:15 +1300

    Added Message of The Day command, Notice command and PrivateMessage command

---
+++ JabberIRC/IRC/Extensions/MotdExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JabberIRC.IRC.Extensions
+{
+    // Motd stands for Message of the Day
+    public static class MotdExtension
+    {
+        // Get the message of the day of the given server. If there is no target given it will return the MOTD of the current server
+        public static void MessageOfTheDay(this ApiWrapper apiWrapper, string target=null, StreamWriter writer)
+        {
+            writer.WriteLine($"MOTD{target ?? $" {target}"}");
+        } 
+    }
+}
