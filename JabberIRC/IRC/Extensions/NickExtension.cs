74f64fead9d9aefd8ff996d1daaf99f737cf56f2
Fernando Zhu <fernandoisadev@gmail.com>
2018-01-30 13:53:15 +1300

    Added Nick, Pass, Quit, Service, Squit, User commands

---
+++ JabberIRC/IRC/Extensions/NickExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace JabberIRC.IRC.Extensions
+{
+    /// <summary>
+    /// Parameters: nickname
+    /// 
+    /// NICK command is used to give user a nickname or change the existing
+    /// one.
+    /// </summary>
+    public static class NickExtension
+    {
+        public static void Nick(this ApiWrapper apiWrapper, StreamWriter writer, string nickname)
+        {
+            writer.WriteLine($"NICK {nickname}");
+        }
+    }
+}
